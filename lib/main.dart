5954e0df67a080fcae825fd8ba411d1d605851cb
Mariyam Ghani <mariyamafshaanghani@gmail.com>
2021-05-19 19:22:36 +0530

    v3.0

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:admin/constants.dart';
 import 'package:admin/screens/home/home_screen.dart';
 // import 'package:admin/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'package:provider/provider.dart';
 
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fyrwatch',
       theme: ThemeData.dark().copyWith(
