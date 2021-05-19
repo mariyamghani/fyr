a8edb1ee6d088a21a0fe8a32be72c793fdfad900
Mariyam Ghani <mariyamafshaanghani@gmail.com>
2021-05-19 18:48:25 +0530

    v3.0

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:admin/constants.dart';
+// import 'package:admin/controllers/MenuController.dart';
+import 'package:admin/screens/home/home_screen.dart';
+// import 'package:admin/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fyrwatch',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: HomeScreen(),
+      // navigatorKey: Get.to(Mao),
+
+      // MultiProvider(
+      //   providers: [
+      //     ChangeNotifierProvider(
+      //       create: (context) => MenuController(),
+      //     ),
+      //   ],
+      //   child: MainScreen(),
+      // ),
+    );
+  }
+}
